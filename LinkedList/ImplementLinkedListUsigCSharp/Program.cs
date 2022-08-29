66dbfbc719304828209df1112ee47c60944d3f0a
shohag66 <professional.cse66@gmail.com>
2022-08-30 03:23:13 +0600

    ImplementSimpleLinkedList

--- LinkedList/ImplementLinkedListUsigCSharp/Program.cs
+++ LinkedList/ImplementLinkedListUsigCSharp/Program.cs
@@ -1,2 +1,85 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace _ImplementLinkedListUsigCSharp
+{
+    //node structure
+    class Node
+    {
+        public int data;
+        public Node next;
+    };
+
+    class LinkedList
+    {
+        public Node head;
+        //constructor to create an empty LinkedList
+        public LinkedList()
+        {
+            head = null;
+        }
+
+
+        //display the content of the list
+        public void DisPlayList()
+        {
+            Node temp = new Node();
+            temp = this.head;
+            if (temp != null)
+            {
+                Console.Write("The list contains: ");
+                while (temp != null)
+                {
+                    Console.Write(temp.data + "->");
+                    temp = temp.next;
+                }
+                Console.WriteLine("NULL");
+            }
+            else
+            {
+                Console.WriteLine("The list is empty.");
+            }
+        }
+
+
+    };
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////create an empty LinkedList
+            LinkedList MyList = new LinkedList();
+
+            //Add first node.
+            Node first = new Node();
+            first.data = 10;
+            first.next = null;
+            //linking with head node
+            MyList.head = first;
+
+            //Add second node.
+            Node second = new Node();
+            second.data = 20;
+            second.next = null;
+            //linking with first node
+            first.next = second;
+
+            //Add third node.
+            Node third = new Node();
+            third.data = 30;
+            third.next = null;
+            //linking with second node
+            second.next = third;
+
+
+            //print the content of list
+            MyList.DisPlayList();
+
+        }
+    }
+}
+
+
